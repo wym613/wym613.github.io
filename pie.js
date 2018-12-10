9d292a7a074ccf1ef1f54a88b9928bba8d7815f9
Yimeng Wang <31758925+wym613@users.noreply.github.com>
2018-12-10 06:41:28 -0500

    Update pie.js

--- pie.js
+++ pie.js
@@ -13,6 +13,18 @@ function addSlice(sliceSize, pieElement, offset, sliceID, color) {
     "background-color": color
   });
 }
+function addSlice2(sliceSize, pieElement, offset, sliceID, color) {
+  $(pieElement).append("<div class='slice2 "+sliceID+"'><span></span></div>");
+  var offset = offset - 1;
+  var sizeRotation = -179 + sliceSize;
+  $("."+sliceID).css({
+    "transform": "rotate("+offset+"deg) translate3d(0,0,0)"
+  });
+  $("."+sliceID+" span").css({
+    "transform"       : "rotate("+sizeRotation+"deg) translate3d(0,0,0)",
+    "background-color": color
+  });
+}
 function iterateSlices(sliceSize, pieElement, offset, dataCount, sliceCount, color) {
   var sliceID = "s"+dataCount+"-"+sliceCount;
   var maxSize = 179;
@@ -21,6 +33,17 @@ function iterateSlices(sliceSize, pieElement, offset, dataCount, sliceCount, col
   } else {
     addSlice(maxSize, pieElement, offset, sliceID, color);
     iterateSlices(sliceSize-maxSize, pieElement, offset+maxSize, dataCount, sliceCount+1, color);
+  }
+}
+
+function iterateSlices2(sliceSize, pieElement, offset, dataCount, sliceCount, color) {
+  var sliceID = "s"+dataCount+"-"+sliceCount;
+  var maxSize = 179;
+  if(sliceSize<=maxSize) {
+    addSlice2(sliceSize, pieElement, offset, sliceID, color);
+  } else {
+    addSlice2(maxSize, pieElement, offset, sliceID, color);
+    iterateSlices2(sliceSize-maxSize, pieElement, offset+maxSize, dataCount, sliceCount+1, color);
   }
 }
 function createPie(dataElement, pieElement) {
@@ -52,3 +75,34 @@ function createPie(dataElement, pieElement) {
     offset += size;
   }
 }
+
+function createPie2(dataElement, pieElement) {
+  var listData = [];
+  $(dataElement+" span").each(function() {
+    listData.push(Number($(this).html()));
+  });
+  var listTotal = 0;
+  for(var i=0; i<listData.length; i++) {
+    listTotal += listData[i];
+  }
+  var offset = 0;
+  var color = [
+    "cornflowerblue", 
+    "olivedrab", 
+    "orange", 
+    "tomato", 
+    "crimson", 
+    "purple", 
+    "turquoise", 
+    "forestgreen", 
+    "navy", 
+    "gray"
+  ];
+  for(var i=0; i<listData.length; i++) {
+    var size = sliceSize(listData[i], listTotal);
+    iterateSlices(size, pieElement, offset, i, 0, color[i]);
+    $(dataElement+" li:nth-child("+(i+1)+")").css("border-color", color[i]);
+    offset += size;
+  }
+}
+
